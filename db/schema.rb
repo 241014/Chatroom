6795f646ec1846db02292ac31a45778c280480c6
Araceli <araceli.kim96@gmail.com>
2017-08-08 12:46:26 -0500

    chatroom

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170807010535) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chats", force: :cascade do |t|
     t.string "user"
     t.string "message"
